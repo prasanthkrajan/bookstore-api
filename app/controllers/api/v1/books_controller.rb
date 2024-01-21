abcd7749f7cd8c813e3af169dcd5a1119994765b
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2024-01-22 02:23:47 +0800

    add api docs

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,27 +1,44 @@
 class Api::V1::BooksController < Api::V1::BaseController
 	include ErrorHandler
-
 	before_action :set_book, only: %i[show update destroy]	
 
+	api :GET, '/v1/books', 'To retrieve all books'
 	def index
 		@books = Book.all
 		render json: @books, status: :ok
 	end
 
+	api :GET, '/v1/books/:id', 'To retrieve a single book, by providing book ID'
+  param :id, Numeric, desc: 'Book ID', required: true
 	def show
 		render json: @book, status: :ok
 	end
 
+	api :PUT, '/v1/books/:id', 'To update a single book, by providing book ID, and attributes to be updated'
+  param :id, Numeric, desc: 'Book ID', required: true
+  param :title, String, desc: 'Title of book', required: true
+  param :author, String, desc: 'Author of book', required: true
+  param :published_year, String, desc: 'Year book was published', required: true
+  param :isbn_number, String, desc: 'ISBN number of book', required: true
+  param :price, String, desc: 'Price of book', required: false
 	def update
 		@book.update!(book_params)
 		render json: @book, status: :ok
 	end
 
+	api :POST, '/v1/books', 'To create a single book, by providing attributes required'
+  param :title, String, desc: 'Title of book', required: true
+  param :author, String, desc: 'Author of book', required: true
+  param :published_year, String, desc: 'Year book was published', required: true
+  param :isbn_number, String, desc: 'ISBN number of book', required: true
+  param :price, String, desc: 'Price of book', required: false
 	def create
 		@book = Book.create!(book_params)
 		render json: @book, status: :ok
 	end
 
+	api :DELETE, '/v1/books/:id', 'To delete a single book, by providing book ID'
+  param :id, Numeric, desc: 'Book ID', required: true
 	def destroy
 		@book.destroy
 		head :no_content
