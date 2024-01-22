f7f872402ea507a23c20a59b5bd797b45f155455
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2024-01-22 12:11:04 +0800

    add author model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_21_072512) do
+ActiveRecord::Schema.define(version: 2024_01_22_040744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title", null: false
