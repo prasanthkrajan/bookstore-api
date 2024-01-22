b0ffa0cd63967f9df7c2fcf3c0b8b6bd96c54fc7
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2024-01-22 10:06:47 +0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Book.destroy_all
+Book.create([
+	{ title: 'A Gentleman in Moscow', author: 'Amor Towles', published_year: 2000, isbn_number: 'ISBN-20201', price: 15.00 },
+	{ title: '11/22/63', author: 'Stephen King', published_year: 1990, isbn_number: 'ISBN-20202', price: 12.00 },
+	{ title: 'How to Win', author: 'New Author', published_year: 2000, isbn_number: 'ISBN-20203', price: 15.00 },
+	{ title: 'Life of Pi', author: 'Anonymous', published_year: 2010, isbn_number: 'ISBN-20204', price: 45.00 },
+	{ title: 'Great Wall', author: 'Another Author', published_year: 2000, isbn_number: 'ISBN-20205', price: 37.00 }
+])
