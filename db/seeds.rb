836fa38356699b206af8ab612293bc9ec87f26f1
Prasanth K. Rajan <prasanth.k.rajan@gmail.com>
2024-01-22 14:39:45 +0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 Book.destroy_all
-Book.create([
-	{ title: 'A Gentleman in Moscow', author: 'Amor Towles', published_year: 2000, isbn_number: 'ISBN-20201', price: 15.00 },
-	{ title: '11/22/63', author: 'Stephen King', published_year: 1990, isbn_number: 'ISBN-20202', price: 12.00 },
-	{ title: 'How to Win', author: 'New Author', published_year: 2000, isbn_number: 'ISBN-20203', price: 15.00 },
-	{ title: 'Life of Pi', author: 'Anonymous', published_year: 2010, isbn_number: 'ISBN-20204', price: 45.00 },
-	{ title: 'Great Wall', author: 'Another Author', published_year: 2000, isbn_number: 'ISBN-20205', price: 37.00 }
-])
+Author.destroy_all
+
+10.times do |i|
+	author = Author.create(name: Faker::Name.name, location: Faker::Address.city)
+	10.times do |ii|
+		Book.create(title: Faker::Book.title, 
+								author: author, 
+								published_year: Faker::Number.within(range: 1900..2023), 
+								isbn_number: "ISBN #{Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3)}", 
+								price: Faker::Number.decimal)
+	end
+end
--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :author do
-    name { Faker::Name.name}
+    name { Faker::Name.name }
     location { Faker::Address.city }
   end
 end
